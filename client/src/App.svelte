e8ef1f66389f851f4e369722fdab1e95a677f434
Julaam Diop <jeebe.diop@gmail.com>
2021-11-21 18:33:34 -0600

    Create functions and html for name change and age increment

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
-	export let name: string;
+	let name: string = "Johnny";
+	let age: number = 21;
+
+	// Svelte version of "labeled statements"
+	$: uppercaseName = name.toUpperCase();
+
+	function incrementAge():void {
+		age += 1
+	}
+
+	function changeName():void {
+		name = "Not Johnny"
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {uppercaseName}!</h1>
+	<p>I am {age} years old! </p>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={incrementAge}> Change Age </button>
+	<button on:click={changeName}> Change Name</button>
 </main>
 
 <style lang="scss">
-	$color: green;
+	$color: blue;
 
 	h1 {
 		color: $color;
